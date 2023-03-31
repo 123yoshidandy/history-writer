9748b1699de8eaad5218999bd917b67ebf542105
kazuki.yoshida <kazuki_6499@yahoo.co.jp>
2023-03-31 10:51:39 +0900

    mode切り替え機能を導入

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+let mode = "";
+
+window.addEventListener('load', init);
+
+async function init() {
+    let param = location.search;
+    console.log("param: " + param);
+    console.log("param.search(yoshida): " + param.search("yoshida"));
+
+    if (param.search("yoshida") != -1 || param.search("family") != -1) {
+        mode = "family";
+        document.getElementById("title").textContent = "History Writer for YoshidaFamily";
+    } else if (param.search("mayuka") != -1) {
+        mode = "mayuka";
+        document.getElementById("title").textContent = "History Writer for MayuKazuki";
+    } else {
+        mode = "kazuki";
+        document.getElementById("title").textContent = "History Writer for Kazuki";
+    }
+    console.log("mode: " + mode);
+}
+
 async function onSubmit() {
     let date = document.getElementById("date").value;
     let category = document.getElementById("category").value;
@@ -13,6 +35,6 @@ async function onSubmit() {
 
 function addEvent(date, category, event) {
     let url = "https://script.google.com/macros/s/AKfycbyUTm7xSliKvWCNt4uPOhE9WAN8XL47akztX5k_zK2MjA1druCEsezOzcAOCMqGdnnO/exec";
-    url = url + "?date=" + date + "&category=" + category + "&event=" + event
+    url = url + "?mode=" + mode + "&date=" + date + "&category=" + category + "&event=" + event
     return fetch(url, { mode: "no-cors" }).then(response => {});
 }
